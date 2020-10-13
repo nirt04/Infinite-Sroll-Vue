512b30785e89034696edd37ad85238ef9379c6cf
Nir <nirt04@gmail.com>
2020-10-13 21:10:32 +0300

    refactor

--- src/views/ListPage.vue
+++ src/views/ListPage.vue
@@ -15,7 +15,6 @@ export default {
   methods: {
     getItems(itemsCount, page) {
       const curItemIndex = page === 1 ? 0 : (page - 1) * (this.ROW_LIST.length / 5);
-      debugger;
       return [...this.ROW_LIST].splice(curItemIndex, itemsCount);
     },
     infiniteHandler($state) {
@@ -23,10 +22,8 @@ export default {
       const itemsCount = this.ROW_LIST.length / 5;
       setTimeout(() => {
         const newItems = this.getItems(itemsCount, that.page++);
-
         if (newItems.length > 0) {
           that.arrayToRender.push(...newItems);
-          // that.page = that.page + 1;
           $state.loaded();
         } else {
           $state.complete();
