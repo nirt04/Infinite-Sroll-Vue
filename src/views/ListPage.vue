a065a3e8f7ea1b3fecce6363e8620c136b534df9
Nir <nirt04@gmail.com>
2020-10-13 21:09:54 +0300

    refactor

--- src/views/ListPage.vue
+++ src/views/ListPage.vue
@@ -10,20 +10,23 @@ import InfiniteLoading from "vue-infinite-loading";
 import ListContainer from "./components/list-container.vue";
 
 export default {
+  /* eslint-disable */
+
   methods: {
     getItems(itemsCount, page) {
-      const curItemIndex = page === 1 ? 0 : page * (this.ROW_LIST.length / 5);
+      const curItemIndex = page === 1 ? 0 : (page - 1) * (this.ROW_LIST.length / 5);
+      debugger;
       return [...this.ROW_LIST].splice(curItemIndex, itemsCount);
     },
     infiniteHandler($state) {
       const that = this;
       const itemsCount = this.ROW_LIST.length / 5;
       setTimeout(() => {
-        const newItems = this.getItems(itemsCount, that.page);
+        const newItems = this.getItems(itemsCount, that.page++);
 
         if (newItems.length > 0) {
           that.arrayToRender.push(...newItems);
-          that.page = that.page + 1;
+          // that.page = that.page + 1;
           $state.loaded();
         } else {
           $state.complete();
